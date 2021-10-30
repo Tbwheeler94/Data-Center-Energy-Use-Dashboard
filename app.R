ba9be455d60deb877803d6e48c39187b0cdd1568
Thomas Wheeler <tbwheeler94@gmail.com>
2021-10-30 13:08:23 -0700

    fixed app

--- app.R
+++ app.R
@@ -4,29 +4,19 @@ library(tidyverse)
 library(lubridate)
 library(shiny)
 
-## Sources
-#source("ui.R")
-#source("server.R")
-
-## Source files
-#source(here("R", "companyOutput.R"))
-
 data_sheet <- read.csv(here('data', 'practice_data.csv'))
 
 
 ui <- htmlTemplate(filename = "www/index.html",
-                   company_graph = plotOutput(outputId = "companyPlot"))
+                   company_graph = plotOutput("companyPlot"))
 
 #Server code
 server <- function(input, output) {
   
   # Annual impact globe output
   output$companyPlot <- renderPlot({
-    p1 <- ggplot(data = data_sheet, aes(fill=company, y=value, x=data_year)) + 
-      geom_bar(stat="identity") +
-      theme(legend.position="none")
-    
-    p1
+    ggplot(data = data_sheet, aes(fill=company, y=value, x=data_year)) + 
+      geom_bar(stat="identity")
   })
 }
 
