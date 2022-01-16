4d409217b58b1a5d30ee59384f543958dc8d0828
Thomas Wheeler <tbwheeler94@gmail.com>
2022-01-15 17:30:51 -0800

    built out framework for new UI

--- ui.R
+++ ui.R
@@ -18,6 +18,7 @@ HighlightsCard <- function(...) {
   Stack(
     class = "ms-depth-8",
     tokens = list(padding = 20, childrenGap = 5),
+    style = "text-align: center",
     ...  
   )
 }
@@ -37,41 +38,129 @@ Grid <- function (...) {
 GridItem <- function (..., class = "ms-sm12") {
   div(
     class = paste("ms-Grid-col", class),
-    style = "padding: 10px; text-align: center",
+    style = "padding: 10px;",
     ...
   )
 }
 
-################################
-############## UI ##############
-################################
-ui <- fluentPage(
-  
-  MainCard(title = "About the Data",
-       Text("We are a team of University of California, Santa Barbara based researchers aiming to increase transparency and understanding of trends in global data center energy use. This website is a dashboard for modelers, policy-makers, and the general public to gain insight into data currently being reported by many of the world largest technology companies. Our visualization uses aggregated energy data primarily collected from publicly disclosed corporate sustainability reports.", variant = "large")),
-  
-  Grid(
-    GridItem(class = "ms-sm12 ms-xl4",
-      HighlightsCard(
-        FontIcon(iconName = "Calendar", style = list(fontSize = 60)),
-        Text(uiOutput("years_reported"), variant = "mega"),
-        Text("Years Reporting", variant = "xxLarge")
-      )
-    ),
-    GridItem(class = "ms-sm12 ms-xl4",
-      HighlightsCard(
-        FontIcon(iconName = "ClipboardList", style = list(fontSize = 60)),
-        Text(uiOutput("companies_reporting"), variant = "mega"),
-        Text("Companies Reporting", variant = "xxLarge")
-      )
-    ),
-    GridItem(class = "ms-sm12 ms-xl4",
-      HighlightsCard(
-        FontIcon(iconName = "TableComputed", style = list(fontSize = 60)),
-        Text(uiOutput("energy_reported"), variant = "mega"),
-        Text("Energy Reported", variant = "xxLarge")
-      )
+makePage <- function (contents) {
+  tagList(
+  contents)
+}
+
+#######################################
+############## HOME PAGE ##############
+#######################################
+
+home_page <- makePage(
+  div(
+    Grid(
+      GridItem(class = "ms-sm12 ms-xl12",
+        MainCard(title = "About the Data",
+           Text("We are a team of University of California, Santa Barbara based researchers aiming to increase transparency and understanding of trends in global data center energy use. This website is a dashboard for modelers, policy-makers, and the general public to gain insight into data currently being reported by many of the world largest technology companies. Our visualization uses aggregated energy data primarily collected from publicly disclosed corporate sustainability reports.", variant = "large"))
+        ),
+        GridItem(class = "ms-sm12 ms-xl4",
+          HighlightsCard(
+            FontIcon(iconName = "Calendar", style = list(fontSize = 60)),
+            Text(uiOutput("years_reported"), variant = "mega"),
+            Text("Years Reporting", variant = "xxLarge")
+          )
+        ),
+        GridItem(class = "ms-sm12 ms-xl4",
+          HighlightsCard(
+            FontIcon(iconName = "ClipboardList", style = list(fontSize = 60)),
+            Text(uiOutput("companies_reporting"), variant = "mega"),
+            Text("Companies Reporting", variant = "xxLarge")
+          )
+        ),
+        GridItem(class = "ms-sm12 ms-xl4",
+          HighlightsCard(
+            FontIcon(iconName = "TableComputed", style = list(fontSize = 60)),
+            Text(uiOutput("energy_reported"), variant = "mega"),
+            Text("Energy Reported", variant = "xxLarge")
+          )
+        )
     )
   )
-  
+)
+
+##########################################
+############## DASHBOARD UI ##############
+##########################################
+
+header <- "header"
+navigation <- "navigation"
+footer <- "footer"
+
+layout <- function(mainUI){
+  div(class = "grid-container",
+      div(class = "header", header),
+      div(class = "sidenav", navigation),
+      div(class = "main", mainUI),
+      div(class = "footer", footer)
+  )
+}
+
+header <- tagList(
+  img(src = "ucsb.png", class = "logo"),
+  div(Text(variant = "xLarge", "Energy Use Dashboard"), class = "title"),
+  CommandBar(
+    items = list(
+      CommandBarItem("New", "Add", subitems = list(
+        CommandBarItem("Email message", "Mail", key = "emailMessage", href = "mailto:me@example.com"),
+        CommandBarItem("Calendar event", "Calendar", key = "calendarEvent")
+      )),
+      CommandBarItem("Upload sales plan", "Upload"),
+      CommandBarItem("Share analysis", "Share"),
+      CommandBarItem("Download report", "Download")
+    ),
+    farItems = list(
+      CommandBarItem("Grid view", "Tiles", iconOnly = TRUE),
+      CommandBarItem("Info", "Info", iconOnly = TRUE)
+    ),
+    style = list(width = "100%")))
+
+navigation <- Nav(
+  groups = list(
+    list(links = list(
+      list(name = 'Home', url = '#!/', key = 'home', icon = 'Home'),
+      list(name = 'Industry Trends', url = '#!/other', key = 'analysis', icon = 'AnalyticsReport'),
+      list(name = 'Company Analysis', url = 'http://github.com/Appsilon/shiny.fluent', key = 'repo', icon = 'AnalyticsReport'),
+      list(name = 'Methods', url = 'http://appsilon.com', key = 'appsilon', icon = 'WebAppBuilderFragment')
+    ))
+  ),
+  initialSelectedKey = 'home',
+  styles = list(
+    root = list(
+      height = '100%',
+      boxSizing = 'border-box',
+      overflowY = 'auto'
+    )
+  )
+)
+
+footer <- Stack(
+  horizontal = TRUE,
+  horizontalAlign = 'space-between',
+  tokens = list(childrenGap = 20),
+  Text(variant = "medium", "Built with ❤ by Industrial Sustainability Analysis Lab", block=TRUE),
+  Text(variant = "medium", nowrap = FALSE, "If you'd like to learn more, reach out to us at info_isal@ucsb.edu"),
+  Text(variant = "medium", nowrap = FALSE, "All rights reserved.")
+)
+
+
+layout <- function(mainUI){
+  div(class = "grid-container",
+      div(class = "header", header),
+      div(class = "sidenav", navigation),
+      div(class = "main", mainUI),
+      div(class = "footer", footer)
+  )
+}
+
+ui <- fluentPage(
+  layout(home_page),
+  tags$head(
+    tags$link(href = "main.css", rel = "stylesheet", type = "text/css")
+  )
 )
