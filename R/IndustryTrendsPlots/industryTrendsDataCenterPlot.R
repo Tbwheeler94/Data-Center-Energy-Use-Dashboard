3a37cef89d444583744fa58303bacc2fdbe0498d
isaiahgarcia <isaiahgarcia@umail.ucsb.edu>
2022-03-10 16:34:13 -0800

    ig - took out ggplotly

--- R/IndustryTrendsPlots/industryTrendsDataCenterPlot.R
+++ R/IndustryTrendsPlots/industryTrendsDataCenterPlot.R
@@ -2,7 +2,7 @@ buildIndustryTrendsDataCenterPlot <- function(energy_use_final) {
   
   if ("Data Centers" %in% energy_use_final$energy_reporting_scope){
     # create plot for data center electricity usage
-    dc <- ggplot(energy_use_final, aes(x=electricity_converted)) + 
+    ggplot(energy_use_final, aes(x=electricity_converted)) + 
       geom_bar(aes(y=company, fill=level_of_ownership), 
                position=position_stack(reverse = TRUE), stat="identity") +
       ggtitle("Data Center Electricity Use by Year") +
@@ -13,8 +13,6 @@ buildIndustryTrendsDataCenterPlot <- function(energy_use_final) {
       xlab("Electricity Value (KWh)") +
       ylab("Companies") +
       labs(fill = "Level Of Ownership") 
-    
-    ggplotly(dc)
   }
   
   else{
