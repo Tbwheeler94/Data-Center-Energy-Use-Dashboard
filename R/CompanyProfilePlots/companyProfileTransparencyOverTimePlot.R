e8fe97d3b77e93f6de06f200a508f64f17c434b3
isaiahgarcia <isaiahgarcia@umail.ucsb.edu>
2022-04-15 15:55:45 -0700

    ig - adding timeline plots

---
+++ R/CompanyProfilePlots/companyProfileTransparencyOverTimePlot.R
@@ -0,0 +1,103 @@
+buildCompanyProfileTransparencyOverTimePlot <- function(data_sheet_energy_transformed, selected_company) {
+  for (year in 2007:as.integer(max(na.omit(data_sheet_energy_raw$report_year)))) {
+    # create a sub data frame that is filtered by data year and single data center scope
+    company_SDC <- data_sheet_energy_transformed %>%
+      filter(company == "Oracle", data_year == year, energy_reporting_scope == "Single Data Center") %>%
+      select(company, data_year, energy_reporting_scope, fuel_1_type) 
+    
+    # create a sub data frame that is filtered by data year and multiple data centers scope
+    company_MDC <- data_sheet_energy_transformed %>%
+      filter(company == "Oracle", data_year == year, energy_reporting_scope == "Multiple Data Centers") %>%
+      select(company, data_year, energy_reporting_scope, fuel_1_type)
+    
+    # create a sub data frame that is filtered by data year / company wide electricity scope
+    company_CW <- data_sheet_energy_transformed %>%
+      filter(company == "Oracle", data_year == year, energy_reporting_scope == "Total Operations") %>%
+      select(company, data_year, energy_reporting_scope, fuel_1_type) 
+    
+    company_EN <- data_sheet_energy_transformed %>%
+      filter(company == "Oracle", data_year == year, fuel_1_type == "Total Energy Use") %>%
+      select(company, data_year, energy_reporting_scope, fuel_1_type) 
+    
+    if (year == 2007) {
+      company_transparency <- rbind(company_SDC, company_MDC, company_CW, company_EN)
+       if (nrow(company_transparency) == 0) {
+         company_transparency[nrow(company_transparency)+1, ] <- c("Oracle",year,"","")
+       }
+    } else {
+      company_transparency <- company_transparency %>% rbind(company_SDC, company_MDC, company_CW, company_EN)
+      if (!(year %in% company_transparency)) {
+         company_transparency[nrow(company_transparency)+1, ] <- c("Oracle",year,"","")
+       }
+    }
+  }
+
+  #company_transparency <- company_transparency %>% distinct(company, data_year, .keep_all = TRUE)
+  year_axis <- company_transparency %>% distinct(company, data_year, .keep_all = TRUE) %>% select(-c(company, fuel_1_type))
+  year_axis$energy_reporting_scope[year_axis$energy_reporting_scope == "Single Data Center"] <- "Reported Data Center Electricity"
+  year_axis$energy_reporting_scope[year_axis$energy_reporting_scope == "Multiple Data Centers"] <- "Reported Data Center Electricity"
+  year_axis$energy_reporting_scope[year_axis$energy_reporting_scope == "Total Operations"] <- "Reported Company Wide Electricity"
+  year_axis$energy_reporting_scope[year_axis$fuel_1_type == "Total Energy Use"] <- "Reported Company Wide Energy"
+  year_axis$energy_reporting_scope[year_axis$energy_reporting_scope == ""] <- "No Reporting of Data"
+  
+  company_transparency <- company_transparency %>% distinct(company, data_year, .keep_all = TRUE) %>% distinct(company, energy_reporting_scope, .keep_all = TRUE)
+  company_transparency$energy_reporting_scope[company_transparency$energy_reporting_scope == "Single Data Center"] <- "Reported Data Center Electricity"
+  company_transparency$energy_reporting_scope[company_transparency$energy_reporting_scope == "Multiple Data Centers"] <- "Reported Data Center Electricity"
+  company_transparency$energy_reporting_scope[company_transparency$energy_reporting_scope == "Total Operations"] <- "Reported Company Wide Electricity"
+  company_transparency$energy_reporting_scope[company_transparency$fuel_1_type == "Total Energy Use"] <- "Reported Company Wide Energy"
+  company_transparency$energy_reporting_scope[company_transparency$energy_reporting_scope == ""] <- "No Reporting of Data"
+  company_transparency <- company_transparency %>% select(-c(company, fuel_1_type))
+  
+  company_transparency$position <- 0
+  company_transparency$text_position <- 0
+  position_val <- 0.25
+  factor <- 1.5
+  company_transparency[1,3] <- 0.25
+  company_transparency[1,4] <- 0.27
+  for (i in 2:nrow(company_transparency)) {
+    if (i %% 2 == 0) {
+      position_val <- position_val * factor
+      company_transparency[i,3] <- position_val
+      company_transparency[i,4] <- position_val+0.02
+      factor <- factor / 1.25
+    } else {
+      position_val <- position_val / factor
+      company_transparency[i,3] <- position_val
+      company_transparency[i,4] <- position_val+0.02
+      factor <- factor / 1.25
+    }
+  }
+  
+  status_levels <- c("Reported Data Center Electricity", "Reported Company Wide Electricity", 
+                     "Reported Company Wide Energy", "No Reporting of Data")
+  status_colors <- c("#0070C0", "#00B050", "#FFC000", "#C00000")
+  
+  year_axis$energy_reporting_scope <- factor(year_axis$energy_reporting_scope, levels=status_levels, ordered=TRUE)
+  year_axis$year_text <- as.character(year_axis$data_year)
+  
+  ggplot(data=year_axis, aes(x=data_year, y=0, color=energy_reporting_scope)) +
+    geom_hline(yintercept=0, color="black", size=0.3) +
+    geom_point(data=year_axis, aes(y=0), size=3) +
+    geom_segment(data=company_transparency, aes(y=position,yend=0,xend=data_year), color='black', size=0.2) +
+    geom_text(data=company_transparency, aes(y=text_position, label=energy_reporting_scope), size=2.5) +
+    geom_text(data=year_axis, aes(x=year_text, y=-0.02, label=year_text), color="black", size=2.5) +
+    scale_color_manual(values=status_colors, labels=status_levels, drop = FALSE) +
+    ggtitle("Oracle's Timeline") +
+    theme_classic() +
+    labs(col="Reporting Scope") +
+    theme(axis.line.y=element_blank(),
+          axis.text.y=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.line.x=element_blank(),
+          legend.position = "bottom"
+    )
+  
+  
+  
+  
+  
+}
---
+++ R/IndustryTrendsPlots/industryTrendsTimelinePlot.R
@@ -0,0 +1,53 @@
+buildIndustryTrendsTimelinePlot <- function(data_sheet_energy_transformed) {
+  for (year in 2007:as.integer(max(na.omit(data_sheet_energy_raw$report_year)))) {
+    # create a sub data frame that is filtered by data year and single data center scope
+    company_SDC <- data_sheet_energy_transformed %>%
+      filter(data_year == year, energy_reporting_scope == "Single Data Center") %>%
+      select(company, data_year, energy_reporting_scope, fuel_1_type) 
+    
+    # create a sub data frame that is filtered by data year and multiple data centers scope
+    company_MDC <- data_sheet_energy_transformed %>%
+      filter(data_year == year, energy_reporting_scope == "Multiple Data Centers") %>%
+      select(company, data_year, energy_reporting_scope, fuel_1_type)
+    
+    # create a sub data frame that is filtered by data year / company wide electricity scope
+    company_CW <- data_sheet_energy_transformed %>%
+      filter(data_year == year, energy_reporting_scope == "Total Operations") %>%
+      select(company, data_year, energy_reporting_scope, fuel_1_type) 
+    
+    company_EN <- data_sheet_energy_transformed %>%
+      filter(data_year == year, fuel_1_type == "Total Energy Use") %>%
+      select(company, data_year, energy_reporting_scope, fuel_1_type) 
+    
+    if (year == 2007) {
+      industry_transparency <- rbind(company_SDC, company_MDC, company_CW, company_EN)
+    } else {
+      industry_transparency <- industry_transparency %>% rbind(company_SDC, company_MDC, company_CW, company_EN)
+    }
+  }
+  
+  industry_transparency <- industry_transparency %>% distinct(company, data_year, .keep_all = TRUE)
+  industry_transparency <- dummy_rows(industry_transparency, 
+                                     select_columns = c("company", "data_year"),
+                                     dummy_value = "")
+  industry_transparency <- industry_transparency[order(industry_transparency$data_year), ]
+  industry_transparency$energy_reporting_scope[industry_transparency$energy_reporting_scope == "Single Data Center"] <- "Reported Data Center Electricity"
+  industry_transparency$energy_reporting_scope[industry_transparency$energy_reporting_scope == "Multiple Data Centers"] <- "Reported Data Center Electricity"
+  industry_transparency$energy_reporting_scope[industry_transparency$energy_reporting_scope == "Total Operations"] <- "Reported Company Wide Electricity"
+  industry_transparency$energy_reporting_scope[industry_transparency$fuel_1_type == "Total Energy Use"] <- "Reported Company Wide Energy"
+  industry_transparency$energy_reporting_scope[industry_transparency$energy_reporting_scope == ""] <- "No Reporting of Data"
+  industry_transparency <- industry_transparency %>% select(-c(fuel_1_type))
+  
+  industry_transparency$data_year <- paste0("01/01/", industry_transparency$data_year)
+  industry_transparency$data_year <- as.Date(industry_transparency$data_year, "%d/%m/%Y")
+  
+  p <- ggplot(industry_transparency, aes(x=data_year, y=company)) +
+    geom_tile(aes(fill=energy_reporting_scope), height=0.75) +
+    #geom_bar(aes(fill=energy_reporting_scope), stat="identity", position="stack") +
+    #scale_x_date(breaks = industry_transparency$data_year) +
+    #xlim(as.Date("2007-01-01"), as.Date("2020-12-31")) +
+    theme_classic()
+  
+  ggplotly(p)
+  
+}
