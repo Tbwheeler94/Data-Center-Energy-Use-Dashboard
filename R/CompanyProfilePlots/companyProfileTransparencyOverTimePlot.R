2ab7769076ad5db270e88140a68e87d8cac48698
isaiahgarcia <isaiahgarcia@umail.ucsb.edu>
2022-06-24 13:34:52 -0700

    ig - finally fixed warning on company timeline plot

--- R/CompanyProfilePlots/companyProfileTransparencyOverTimePlot.R
+++ R/CompanyProfilePlots/companyProfileTransparencyOverTimePlot.R
@@ -1,44 +1,44 @@
 buildCompanyProfileTransparencyOverTimePlot <- function(data_sheet_energy_transformed, selected_company) {
-  for (year in 2007:as.integer(max(na.omit(data_sheet_energy_raw$report_year)))) {
+  for (year in 2007:as.integer(max(na.omit(data_sheet_energy_transformed$report_year)))) {
     # create a sub data frame that is filtered by data year and single data center scope
     company_SDC <- data_sheet_energy_transformed %>%
       filter(company %in% selected_company, data_year == year, energy_reporting_scope == "Single Data Center") %>%
-      # filter(company == "Google", data_year == year, energy_reporting_scope == "Single Data Center") %>%
+      # filter(company == "NTT Group", data_year == year, energy_reporting_scope == "Single Data Center") %>%
       select(company, data_year, energy_reporting_scope, fuel_1_type) 
     
     # create a sub data frame that is filtered by data year and multiple data centers scope
     company_MDC <- data_sheet_energy_transformed %>%
       filter(company %in% selected_company, data_year == year, energy_reporting_scope == "Multiple Data Centers") %>%
-      # filter(company == "Google", data_year == year, energy_reporting_scope == "Multiple Data Centers") %>%
+      # filter(company == "NTT Group", data_year == year, energy_reporting_scope == "Multiple Data Centers") %>%
       select(company, data_year, energy_reporting_scope, fuel_1_type)
     
     # create a sub data frame that is filtered by data year / company wide electricity scope
     company_CW <- data_sheet_energy_transformed %>%
       filter(company %in% selected_company, data_year == year, energy_reporting_scope == "Total Operations") %>%
-      # filter(company == "Google", data_year == year, energy_reporting_scope == "Total Operations") %>%
+      # filter(company == "NTT Group", data_year == year, energy_reporting_scope == "Total Operations") %>%
       select(company, data_year, energy_reporting_scope, fuel_1_type) 
     
     company_EN <- data_sheet_energy_transformed %>%
       filter(company %in% selected_company, data_year == year, fuel_1_type == "Total Energy Use") %>%
-      # filter(company == "Google", data_year == year, fuel_1_type == "Total Energy Use") %>%
+      # filter(company == "NTT Group", data_year == year, fuel_1_type == "Total Energy Use") %>%
       select(company, data_year, energy_reporting_scope, fuel_1_type) 
     
     if (year == 2007) {
       company_transparency <- rbind(company_SDC, company_MDC, company_CW, company_EN)
-       if (nrow(company_transparency) == 0) {
-         company_transparency <- data.frame(company=c(selected_company),data_year=c(year),
-                                            energy_reporting_scope=c(""),fuel_1_type=c(""))
-       }
     } else {
       company_transparency <- company_transparency %>% rbind(company_SDC, company_MDC, company_CW, company_EN)
-      if (!(year %in% company_transparency)) {
-         company_transparency[nrow(company_transparency)+1, ] <- c(selected_company,year,"","")
-         # testing
-       }
     }
   }
+  
+  timeline <- 2007:as.integer(max(na.omit(data_sheet_energy_transformed$report_year)))
+  missing_years <- which(!(timeline %in% company_transparency$data_year))
+  if (length(missing_years) != 0) {
+    for (i in 1:length(missing_years)) {
+      company_transparency[nrow(company_transparency)+1, ] <- c(company_transparency[1,1], missing_years[i]+2006, "", "")
+    }
+    company_transparency <- company_transparency[order(company_transparency[,"data_year"]), ]
+  }
 
-  #company_transparency <- company_transparency %>% distinct(company, data_year, .keep_all = TRUE)
   year_axis <- company_transparency %>% distinct(company, data_year, .keep_all = TRUE)
   year_axis$energy_reporting_scope[year_axis$energy_reporting_scope == "Single Data Center"] <- "Reported Data\nCenter Electricity"
   year_axis$energy_reporting_scope[year_axis$energy_reporting_scope == "Multiple Data Centers"] <- "Reported Data\nCenter Electricity"
@@ -50,7 +50,6 @@ buildCompanyProfileTransparencyOverTimePlot <- function(data_sheet_energy_transf
   }
   year_axis <- year_axis %>% select(-c(company, fuel_1_type))
   
-  #company_transparency <- company_transparency %>% distinct(company, data_year, .keep_all = TRUE) %>% distinct(company, energy_reporting_scope, .keep_all = TRUE)
   company_transparency <- company_transparency %>% distinct(company, data_year, .keep_all = TRUE)
   company_transparency$energy_reporting_scope[company_transparency$energy_reporting_scope == "Single Data Center"] <- "Reported Data\nCenter Electricity"
   company_transparency$energy_reporting_scope[company_transparency$energy_reporting_scope == "Multiple Data Centers"] <- "Reported Data\nCenter Electricity"
@@ -92,10 +91,10 @@ buildCompanyProfileTransparencyOverTimePlot <- function(data_sheet_energy_transf
     }
   }
   
-  status_levels <- c("Reported Data\nCenter Electricity", "Reported Company\nWide Electricity", 
+  status_levels <- c("Reported Data\nCenter Electricity", "Reported Company\nWide Electricity",
                      "Reported Company\nWide Energy", "No Reporting\nof Data", "Pending Data\nSubmission")
   status_colors <- c("#0070C0", "#00B050", "#FFC000", "#C00000", "#B88C8C")
-  
+
   year_axis$energy_reporting_scope <- factor(year_axis$energy_reporting_scope, levels=status_levels, ordered=TRUE)
   year_axis$year_text <- as.character(year_axis$data_year)
   
@@ -104,7 +103,7 @@ buildCompanyProfileTransparencyOverTimePlot <- function(data_sheet_energy_transf
     geom_point(data=year_axis, aes(y=0), size=3) +
     geom_segment(data=company_transparency, aes(y=position,yend=0,xend=data_year), color='black', size=0.2) +
     geom_label(data=company_transparency, aes(y=text_position, label=energy_reporting_scope), fill="white", size=5, vjust="inward", label.padding = unit(0.6,"lines")) +
-    geom_text(data=year_axis, aes(x=year_text, y=-0.05, label=year_text), color="black", size=6) +
+    geom_text(data=year_axis, aes(x=data_year, y=-0.05, label=year_text), color="black", size=6) +
     scale_color_manual(values=status_colors, labels=status_levels, drop = FALSE) +
     scale_x_discrete(expand = expansion(mult = 0.1)) +
     theme_classic() +
